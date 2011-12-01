e7e78467ccc5cb53b9b130843d4f3d3d08f3bc4f
Miguel San Román <msanromanv@gmail.com>
2011-12-01 21:45:19 +0100

    Añadido menú principal en el xml. De momento no tienen ningún event listener.

--- src/org/gps/pomodorus/GPSActivity.java
+++ src/org/gps/pomodorus/GPSActivity.java
@@ -2,15 +2,12 @@ package org.gps.pomodorus;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class GPSActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android");
-        setContentView(tv);
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
     }
 }
