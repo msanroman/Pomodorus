9e34e5226bf0cd50d435cfd6abafe2b162761520
unknown <Jordi@Jordi-PC.(none)>
2011-12-03 13:57:36 +0100

    Modificada la crida a les activities, millorada i amb menys linies de codi

--- src/org/gps/pomodorus/CrearTascaActivity.java
+++ src/org/gps/pomodorus/CrearTascaActivity.java
@@ -2,22 +2,12 @@ package org.gps.pomodorus;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 
-public class CrearTascaActivity extends Activity implements OnClickListener {
+public class CrearTascaActivity extends Activity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
+    public void onCreate(Bundle savedInstanceState) {	
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.creartasca);
-        View crearTascaMain = findViewById(R.id.CrearTascaButton);
-        crearTascaMain.setOnClickListener(this);
     }
-	
-	public void onClick(View currentView) {
-
-	}
-
 }
--- src/org/gps/pomodorus/GPSActivity.java
+++ src/org/gps/pomodorus/GPSActivity.java
@@ -1,32 +1,21 @@
 package org.gps.pomodorus;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 
-public class GPSActivity extends Activity implements OnClickListener{
+public class GPSActivity extends Activity {
 
 	/** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
+    public void onCreate(Bundle savedInstanceState) {	
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        View crearTascaMain = findViewById(R.id.CrearTascaMain);
-        crearTascaMain.setOnClickListener(this);
     }
 
-    
-	public void onClick(View currentView) {
-		
-		if(currentView.getId() == R.id.CrearTascaMain){
-			Intent newGameIntent = new Intent(this, CrearTascaActivity.class);
-			startActivity(newGameIntent);
-		}
-	}
-    
-    
+	public void onCrearTascaClick(View button) {
+			Intent newIntent = new Intent(this, CrearTascaActivity.class);
+			startActivity(newIntent);
+	}   
 }
