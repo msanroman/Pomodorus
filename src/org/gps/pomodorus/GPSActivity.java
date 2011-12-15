6ede18c731c1896c6907d07daaba1cfc321e73b5
Jay Kid <cyph3rfox@gmail.com>
2011-12-15 11:53:40 +0100

    Botón de datos de prueba para test anadido

--- src/org/gps/pomodorus/GPSActivity.java
+++ src/org/gps/pomodorus/GPSActivity.java
@@ -1,9 +1,16 @@
 package org.gps.pomodorus;
 
+import java.lang.reflect.Array;
+
+import org.gps.databases.CatTaskDbAdapter;
+import org.gps.databases.CategoryDbAdapter;
+import org.gps.databases.TaskDbAdapter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class GPSActivity extends Activity {
 
@@ -42,5 +49,48 @@ public class GPSActivity extends Activity {
 
         Intent newIntent = new Intent(this, LogInTwitterActivity.class);
         startActivity(newIntent);
+    }
+    
+    public void onFillDatabaseTest(View button) {
+
+    	TaskDbAdapter TaskAdapter = new TaskDbAdapter(this);
+        CategoryDbAdapter CatAdapter = new CategoryDbAdapter(this);
+        CatTaskDbAdapter CatTaskAdapter = new CatTaskDbAdapter(this);
+        TaskAdapter.open();
+        
+        long ids[] = new long[5];
+
+		ids[0] = TaskAdapter.createTask("Tasca1", "Tasca1Descripcio", 3);
+		ids[1] = TaskAdapter.createTask("Tasca2", "Tasca2Descripcio", 4);
+		ids[2] = TaskAdapter.createTask("Tasca3", "Tasca3Descripcio", 5);
+		ids[3] = TaskAdapter.createTask("Tasca4", "Tasca4Descripcio", 6);
+		ids[4] = TaskAdapter.createTask("Tasca5", "Tasca5Descripcio", 7);
+		
+		TaskAdapter.close();
+		
+		CatAdapter.open();
+        
+        long idsCat[] = new long[4];
+
+        idsCat[0] = CatAdapter.createCategory("Cat1");
+        idsCat[1] = CatAdapter.createCategory("Cat2");
+        idsCat[2] = CatAdapter.createCategory("Cat3");
+        idsCat[3] = CatAdapter.createCategory("Cat4");
+		
+        CatAdapter.close();
+		
+        CatTaskAdapter.open();
+
+        CatTaskAdapter.createCatTask(idsCat[0], ids[1]);
+        CatTaskAdapter.createCatTask(idsCat[1], ids[1]);
+        CatTaskAdapter.createCatTask(idsCat[2], ids[1]);
+        CatTaskAdapter.createCatTask(idsCat[3], ids[1]);
+        CatTaskAdapter.createCatTask(idsCat[1], ids[2]);
+        CatTaskAdapter.createCatTask(idsCat[3], ids[2]);
+        CatTaskAdapter.createCatTask(idsCat[0], ids[3]);
+        CatTaskAdapter.createCatTask(idsCat[2], ids[3]);
+        CatTaskAdapter.createCatTask(idsCat[2], ids[4]);
+		
+        CatTaskAdapter.close();
     }
 }
